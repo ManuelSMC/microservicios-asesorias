c6aee7b6ed1473ed74047db574c33b37042c8939
DdemonXSoulEdge <palomillo.flores@gmail.com>
2025-11-09 16:55:04 -0600

    Das shit

--- src/main/java/com/asesorias/microservicio_asesorias/entity/Asesoria.java
+++ src/main/java/com/asesorias/microservicio_asesorias/entity/Asesoria.java
@@ -1,30 +1,39 @@
 package com.asesorias.microservicio_asesorias.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "asesoria")
 @Data
 public class Asesoria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_asesoria")
     private Integer idAsesoria;
 
-    @Column(name = "nombre")
-    private String nombre;
+    @Column(name = "tema")
+    private String tema;
 
     @Column(name = "descripcion")
     private String descripcion;
 
+    @Column(name = "lugar")
+    private String lugar;
+
+    @Column(name = "modalidad")
+    private String modalidad;
+
+    @Column(name = "hora_inicio")
+    private String horaInicio;
+
+    @Column(name = "hora_fin")
+    private String horaFin;
+
+    @Column(name = "fecha_creacion")
+    private String fechaCreacion;
+
     @Column(name = "status")
     private Boolean status;
-
-    
 }
